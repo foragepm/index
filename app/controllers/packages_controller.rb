c17b7581c15e2ef3e96f07221066da3ae96b8145
Andrew Nesbitt <andrewnez@gmail.com>
2021-06-25 14:57:59 +0100

    fix package import

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,9 +1,12 @@
 class PackagesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token, only: [:import]
+
   def import
     if params[:api_key] == ENV['IMPORT_API_KEY']
       ImportPackageWorker.perform_async(params[:platform], params[:name])
     end
 
-    render :ok
+    head :ok
   end
 end
