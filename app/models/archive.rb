3786f46b854a9976379aa335fad824f8def2d568
Andrew Nesbitt <andrewnez@gmail.com>
2021-08-02 11:56:36 +0100

    include archive id in web3 filename

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -18,7 +18,7 @@ class Archive < ApplicationRecord
 
   def pin_to_web3_storage
     return if size > 99.megabyte
-    url = "#{ENV['TRANSPORTER_URL']}/?cid=#{cid}&filename=#{filename}"
+    url = "#{ENV['TRANSPORTER_URL']}/?cid=#{cid}&filename=#{id}-#{filename}"
     response = Faraday.get(url)
     if response.success?
       update_columns(web3: true)
