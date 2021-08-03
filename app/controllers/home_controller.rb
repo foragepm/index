5332755ef2e2025cd1795aea52ccacf94cc64379
Andrew Nesbitt <andrewnez@gmail.com>
2021-08-03 14:50:39 +0100

    show count of packages needing pushing to web3.storage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,6 +7,7 @@ class HomeController < ApplicationController
     @without_archives = Version.where(yanked: false).without_archives.count
     @yanked = Version.where(yanked: true).count
     @not_pinned = Archive.not_pinned.count
+    @not_pinned_web3 = Archive.where(web3: false).count
     @statuses = Archive.pinned.group(:pin_status).count
   end
 end
