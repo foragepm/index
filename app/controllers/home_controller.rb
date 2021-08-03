8b986f330784ea9e7f1c3b5a32f2997efab7bcca
Andrew Nesbitt <andrewnez@gmail.com>
2021-08-03 15:09:54 +0100

    not pinned count shouldn't include yanked packages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,8 @@ class HomeController < ApplicationController
   def stats
     @without_archives = Version.where(yanked: false).without_archives.count
     @yanked = Version.where(yanked: true).count
-    @not_pinned = Archive.not_pinned.count
-    @not_pinned_web3 = Archive.where(web3: false).count
+    @not_pinned = Archive.not_yanked.not_pinned.count
+    @not_pinned_web3 = Archive.not_yanked.where(web3: false).count
     @statuses = Archive.pinned.group(:pin_status).count
   end
 end
