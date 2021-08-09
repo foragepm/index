49dc8082c72a823131b7a0751cab8fd5fe7dc6fb
Andrew Nesbitt <andrewnez@gmail.com>
2021-08-09 16:17:08 +0100

    Tweak recent versions page

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,6 +1,6 @@
 class VersionsController < ApplicationController
   def recent
-    @scope = Version.includes(:package).order('published_at DESC')
+    @scope = Version.includes(:package).order('published_at DESC, created_at DESC')
     @pagy, @versions = pagy_countless(@scope)
   end
 end
