6b3bf09a1ab2bb46fb642777ee10db9d26c37386
Andrew Nesbitt <andrewnez@gmail.com>
2021-09-20 11:42:02 +0100

    removed yanked count from stats page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,6 @@ class HomeController < ApplicationController
 
   def stats
     @without_archives = Version.where(yanked: false).without_archives.count
-    @yanked = Version.where(yanked: true).count
     @not_pinned = Archive.not_yanked.not_pinned.count
     @not_pinned_web3 = Archive.not_yanked.where(web3: false).count
     @statuses = Archive.pinned.not_yanked.where.not(pin_status: 'pinned').group(:pin_status).count
