c86d7468af51acef52615c543b6d42ef9d047010
Andrew Nesbitt <andrewnez@gmail.com>
2021-09-21 12:19:44 +0100

    Handle lowercase platform names

--- app/workers/import_package_worker.rb
+++ app/workers/import_package_worker.rb
@@ -3,6 +3,6 @@ class ImportPackageWorker
   sidekiq_options queue: :critical, lock: :until_executed
 
   def perform(platform, name, version_limit = 20)
-    "PackageManager::#{platform}".constantize.update(name, version_limit)
+    "PackageManager::#{platform.capitalize}".constantize.update(name, version_limit)
   end
 end
