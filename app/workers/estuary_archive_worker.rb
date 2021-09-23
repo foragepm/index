b0b7569eb85e79e5d0a58c602128b35e28ce4fb1
Andrew Nesbitt <andrewnez@gmail.com>
2021-09-23 16:40:40 +0100

    revert changes to estuary background worker

--- app/workers/estuary_archive_worker.rb
+++ app/workers/estuary_archive_worker.rb
@@ -3,9 +3,6 @@ class EstuaryArchiveWorker
   sidekiq_options queue: :default, lock: :until_executed
 
   def perform(archive_id)
-    a = Archive.find_by_id(archive_id)
-    return unless a
-    a.pin_to_web3_storage
-    a.pin
+    Archive.find_by_id(archive_id).try(:pin)
   end
 end
