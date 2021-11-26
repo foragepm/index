359878c9be31d081a0ed50ad88d75e09e447d7f9
Andrew Nesbitt <andrewnez@gmail.com>
2021-11-26 17:32:45 +0000

    Update dependencies

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -5,7 +5,7 @@ require 'pagy/extras/overflow'
 require 'pagy/extras/items'
 require 'pagy/extras/countless'
 
-Pagy::VARS[:overflow] = :last_page
+Pagy::DEFAULT[:overflow] = :last_page
 
-Pagy::VARS[:max_items] = 1000
-Pagy::VARS[:items_param] = :per_page
+Pagy::DEFAULT[:max_items] = 1000
+Pagy::DEFAULT[:items_param] = :per_page
