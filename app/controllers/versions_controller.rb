e7909cca5d974cafdf1915c6d1df3a872286a645
Andrew Nesbitt <andrewnez@gmail.com>
2022-02-11 17:37:07 +0000

    try ordering by id

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,6 +1,6 @@
 class VersionsController < ApplicationController
   def recent
-    @scope = Version.includes(:package).order('published_at DESC NULLS LAST, created_at DESC')
+    @scope = Version.includes(:package)order('id desc')#.order('published_at DESC NULLS LAST, created_at DESC')
     @pagy, @versions = pagy_countless(@scope)
   end
 end
